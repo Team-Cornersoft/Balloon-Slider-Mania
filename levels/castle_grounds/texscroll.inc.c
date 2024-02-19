4e7131b1b0d674912076024fb3fc83eb525c4913
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-02-19 23:34:59 +0100

    Example menu in castle couryard

---
+++ levels/castle_grounds/texscroll.inc.c
@@ -0,0 +1,216 @@
+void scroll_castle_grounds_dl_A_Collision_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 4;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_A_Collision_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.25 * 0x20) % width;
+	deltaY = (int)(-0.25 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_B_Menu_checkers_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_B_Menu_checkers_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.25 * 0x20) % width;
+	deltaY = (int)(-0.25 * 0x20) % height;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentX += deltaX;	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_0() {
+	int i = 0;
+	int count = 16;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_0);
+
+	deltaY = (int)(-1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_1() {
+	int i = 0;
+	int count = 8;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_1);
+
+	deltaY = (int)(-1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_2() {
+	int i = 0;
+	int count = 16;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_2);
+
+	deltaY = (int)(-1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_3() {
+	int i = 0;
+	int count = 8;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_3);
+
+	deltaY = (int)(-1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_4() {
+	int i = 0;
+	int count = 8;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_4);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_5() {
+	int i = 0;
+	int count = 12;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_5);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_6() {
+	int i = 0;
+	int count = 12;
+	int height = 64 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_6);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_castle_grounds() {
+	scroll_castle_grounds_dl_A_Collision_mesh_layer_5_vtx_0();
+	scroll_castle_grounds_dl_B_Menu_checkers_mesh_layer_5_vtx_0();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_0();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_1();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_2();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_3();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_4();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_5();
+	scroll_castle_grounds_dl_C_Menu_decoration_mesh_layer_4_vtx_6();
+};
