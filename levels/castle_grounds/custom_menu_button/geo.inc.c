5db4ab1c42cadd3aff9f8007838d9589ba1f5f90
Gregory Heskett <gheskett@gmail.com>
2024-02-22 23:59:17 -0500

    Translate buttons for centered scaling

--- levels/castle_grounds/custom_menu_button/geo.inc.c
+++ levels/castle_grounds/custom_menu_button/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout custom_menu_button_geo[] = {
-	GEO_NODE_START(),
+	GEO_TRANSLATE(LAYER_OPAQUE, 0, -118, 0),
 	GEO_OPEN_NODE(),
 		GEO_ASM(GEO_TRANSPARENCY_MODE_NORMAL, geo_bsm_menu_set_envcolor),
 		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, custom_menu_button_model_mesh_layer_5),
--- levels/castle_grounds/custom_menu_stage/geo.inc.c
+++ levels/castle_grounds/custom_menu_stage/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout custom_menu_stage_geo[] = {
-	GEO_NODE_START(),
+	GEO_TRANSLATE(LAYER_OPAQUE, 0, -210, 0),
 	GEO_OPEN_NODE(),
 		GEO_OPEN_NODE(),
 			GEO_DISPLAY_LIST(LAYER_TRANSPARENT, custom_menu_stage_model_mesh_layer_5_before),
