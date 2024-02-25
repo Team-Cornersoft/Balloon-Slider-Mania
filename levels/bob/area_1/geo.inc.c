c1592f361dacfd8c68480bfc30b145ae338dd2f0
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-02-25 22:23:50 +0100

    Betah track 1 (BOB)

---
+++ levels/bob/area_1/geo.inc.c
@@ -0,0 +1,29 @@
+#include "src/game/envfx_snow.h"
+
+const GeoLayout bob_area_1_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, 0, -5905, 0, 0, 10, 0, bob_dl_0_SKY_DOME_mesh_layer_1),
+		GEO_BILLBOARD_WITH_PARAMS_AND_DL(LAYER_TRANSPARENT, 30000, 31100, 16000, bob_dl_0_SKY_SUN_mesh_layer_5),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, bob_dl_A_area1_slide_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout bob_area_1[] = {
+	GEO_NODE_SCREEN_AREA(10, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+	GEO_OPEN_NODE(),
+		GEO_ZBUFFER(1),
+		GEO_OPEN_NODE(),
+			GEO_CAMERA_FRUSTUM_WITH_FUNC(45.0000, 127, 65535, geo_camera_fov),
+			GEO_OPEN_NODE(),
+				GEO_CAMERA(CAMERA_MODE_8_DIRECTIONS, 0, 0, 0, 0, -100, 0, geo_camera_main),
+				GEO_OPEN_NODE(),
+					GEO_BRANCH(1, bob_area_1_geo),
+					GEO_RENDER_OBJ(),
+					GEO_ASM(ENVFX_MODE_NONE, geo_envfx_main),
+				GEO_CLOSE_NODE(),
+			GEO_CLOSE_NODE(),
+		GEO_CLOSE_NODE(),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
---
+++ levels/bob/area_3/collision.inc.c
@@ -0,0 +1,6 @@
+const Collision bob_area_3_collision[] = {
+	COL_INIT(),
+	COL_VERTEX_INIT(0),
+	COL_TRI_STOP(),
+	COL_END()
+};
