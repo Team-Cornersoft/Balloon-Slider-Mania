12a848bf6599cf7b45d57c09886bb3f86533122a
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-02-27 02:11:57 +0100

    Track 1 wip

--- levels/bob/leveldata.inc.c
+++ levels/bob/leveldata.inc.c
@@ -1,4 +1,5 @@
 #include "levels/bob/area_1/collision.inc.c"
+#include "levels/bob/area_1/room.inc.c"
 #include "levels/bob/area_1/macro.inc.c"
 #include "levels/bob/area_1/spline.inc.c"
 #include "levels/bob/area_2/collision.inc.c"
