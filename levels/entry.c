6d3642f3c5e6ec0da473f019e14de3c8fe38f81b
Gregory Heskett <gheskett@gmail.com>
2024-02-28 03:27:11 -0500

    Bypass scam screen and title screen with debug level select

--- levels/entry.c
+++ levels/entry.c
@@ -3,6 +3,7 @@
 #include "segment_symbols.h"
 #include "level_commands.h"
 #include "game/game_init.h"
+#include "levels/castle_grounds/header.h"
 
 #include "levels/intro/header.h"
 
@@ -16,9 +17,12 @@ const LevelScript level_script_entry[] = {
     SLEEP(/*frames*/ 2),
     CALL_LOOP(/*arg*/ 0, /*func*/ check_fbe),
     BLACKOUT(/*active*/ FALSE),
-#ifdef TEST_LEVEL
+#if defined(TEST_LEVEL)
     SET_REG(/*value*/ TEST_LEVEL),
     EXECUTE(/*seg*/ SEGMENT_GLOBAL_LEVEL_SCRIPT, /*script*/ _scriptsSegmentRomStart, /*scriptEnd*/ _scriptsSegmentRomEnd, /*entry*/ level_main_scripts_entry),
+#elif defined(DEBUG_LEVEL_SELECT)
+    SET_REG(/*value*/ LEVEL_CASTLE_GROUNDS),
+    EXECUTE_WITH_CODE(/*seg*/ SEGMENT_LEVEL_SCRIPT, _castle_groundsSegmentRomStart, _castle_groundsSegmentRomEnd, level_cgds_menu_select, _castle_groundsSegmentBssStart, _castle_groundsSegmentBssEnd),
 #else
     SET_REG(/*value*/ 0),
     EXECUTE(/*seg*/ SEGMENT_MENU_INTRO, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_scam_warning_screen),
