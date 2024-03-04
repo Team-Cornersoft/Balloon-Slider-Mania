35715eea4481bbe88d00bac6dc11964a510d2032
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-03-04 03:48:51 +0100

    Track 1 update! I expect this not to run well

---
+++ levels/bob/texscroll.inc.h
@@ -0,0 +1,4 @@
+extern void scroll_bob_dl_C_finish_line_a4_mesh_layer_5_vtx_0();
+extern void scroll_bob_dl_C_finish_line_a4_mesh_layer_5_vtx_1();
+extern void scroll_bob_dl_C_finish_line_a4_mesh_layer_5_vtx_2();
+extern void scroll_bob();
