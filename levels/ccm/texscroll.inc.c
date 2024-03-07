5550162e1240d928ef0224ec9bfcffff93d65f68
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-03-07 05:08:01 +0100

    Track 4 progress.

--- levels/ccm/texscroll.inc.c
+++ levels/ccm/texscroll.inc.c
@@ -1,11 +1,95 @@
-void scroll_ccm_dl_A_Road_mesh_layer_1_vtx_0() {
+void scroll_ccm_dl_A_Road_mesh_layer_1_vtx_3() {
 	int i = 0;
 	int count = 5249;
 	int width = 32 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	Vtx *vertices = segmented_to_virtual(ccm_dl_A_Road_mesh_layer_1_vtx_0);
+	Vtx *vertices = segmented_to_virtual(ccm_dl_A_Road_mesh_layer_1_vtx_3);
+
+	deltaX = (int)(0.10000000149011612 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_ccm_dl_C_railings_a_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 171;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_C_railings_a_mesh_layer_1_vtx_0);
+
+	deltaX = (int)(0.10000000149011612 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_ccm_dl_C_railings_b_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 258;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_C_railings_b_mesh_layer_1_vtx_0);
+
+	deltaX = (int)(0.10000000149011612 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_ccm_dl_C_railings_c_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 172;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_C_railings_c_mesh_layer_1_vtx_0);
+
+	deltaX = (int)(0.10000000149011612 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_ccm_dl_C_railings_d_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 258;
+	int width = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(ccm_dl_C_railings_d_mesh_layer_1_vtx_0);
 
 	deltaX = (int)(0.10000000149011612 * 0x20) % width;
 
@@ -27,7 +111,20 @@ void scroll_gfx_mat_ccm_dl_Starry_floor_layer1() {
 
 };
 
+void scroll_gfx_mat_ccm_dl_Starry_tiles_layer1() {
+	Gfx *mat = segmented_to_virtual(mat_ccm_dl_Starry_tiles_layer1);
+
+	shift_s(mat, 10, PACK_TILESIZE(0, 1));
+	shift_t(mat, 10, PACK_TILESIZE(0, 1));
+
+};
+
 void scroll_ccm() {
-	scroll_ccm_dl_A_Road_mesh_layer_1_vtx_0();
+	scroll_ccm_dl_A_Road_mesh_layer_1_vtx_3();
+	scroll_ccm_dl_C_railings_a_mesh_layer_1_vtx_0();
+	scroll_ccm_dl_C_railings_b_mesh_layer_1_vtx_0();
+	scroll_ccm_dl_C_railings_c_mesh_layer_1_vtx_0();
+	scroll_ccm_dl_C_railings_d_mesh_layer_1_vtx_0();
 	scroll_gfx_mat_ccm_dl_Starry_floor_layer1();
+	scroll_gfx_mat_ccm_dl_Starry_tiles_layer1();
 };
--- levels/ccm/texscroll.inc.h
+++ levels/ccm/texscroll.inc.h
@@ -1,3 +1,8 @@
-extern void scroll_ccm_dl_A_Road_mesh_layer_1_vtx_0();
+extern void scroll_ccm_dl_A_Road_mesh_layer_1_vtx_3();
+extern void scroll_ccm_dl_C_railings_a_mesh_layer_1_vtx_0();
+extern void scroll_ccm_dl_C_railings_b_mesh_layer_1_vtx_0();
+extern void scroll_ccm_dl_C_railings_c_mesh_layer_1_vtx_0();
+extern void scroll_ccm_dl_C_railings_d_mesh_layer_1_vtx_0();
 extern void scroll_gfx_mat_ccm_dl_Starry_floor_layer1();
+extern void scroll_gfx_mat_ccm_dl_Starry_tiles_layer1();
 extern void scroll_ccm();
