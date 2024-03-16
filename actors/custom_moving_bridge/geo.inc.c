929c1c1e4b57b2da0e9816da89e1d8951f91b7df
Gregory Heskett <gheskett@gmail.com>
2024-03-16 18:03:23 -0400

    Bridges that actually work

--- actors/custom_moving_bridge/geo.inc.c
+++ actors/custom_moving_bridge/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout custom_moving_bridge_geo[] = {
-	GEO_NODE_START(),
+	GEO_CULLING_RADIUS(0x7FFF),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, custom_moving_bridge_Rotatable_platform_mesh_layer_1),
 	GEO_CLOSE_NODE(),
--- src/game/behaviors/rotating_bridge.inc.c
+++ src/game/behaviors/rotating_bridge.inc.c
@@ -1,5 +1,5 @@
 #define BRIDGE_LENGTH 5800.0f
-#define BRIDGE_STARTING_ANGLE -0x2000
+#define BRIDGE_STARTING_ANGLE 0x6000
 #define BRIDGE_MOVE_LENGTH 0x4000
 
 enum RotatingBridgeActions {
@@ -7,27 +7,47 @@ enum RotatingBridgeActions {
     ROTATING_BRIDGE_MOVING,
 };
 
-static void rotating_bridge_move_tmp_for_mist_particles(void) {
-    vec3f_copy(&o->oPosVec, &o->oHomeVec);
+static void rotating_bridge_update_child_balloon(void) {
+    struct Object *obj = o->oRotatingBridgeBalloonChild;
+    if (!obj || obj->activeFlags == ACTIVE_FLAG_DEACTIVATED) {
+        o->oRotatingBridgeBalloonChild = NULL;
+        return;
+    }
+    
+    obj->oPtBalloonRelativePosX = obj->oHomeX + (BRIDGE_LENGTH * 2 / 5) * sins(o->oFaceAngleYaw + BRIDGE_STARTING_ANGLE);
+    obj->oPtBalloonRelativePosY = obj->oHomeY - 100.0f;
+    obj->oPtBalloonRelativePosZ = obj->oHomeZ + (BRIDGE_LENGTH * 2 / 5) * coss(o->oFaceAngleYaw + BRIDGE_STARTING_ANGLE);
+
+    obj->oFaceAngleYaw = o->oFaceAngleYaw + BRIDGE_STARTING_ANGLE;
 }
 
 void bhv_rotating_bridge_init(void) {
+    o->oRotatingBridgeYawHome = o->oFaceAngleYaw;
 
+    struct Object *obj = spawn_object(o, MODEL_BSM_POINT_BALLOON, bhvPointBalloon);
+    if (obj) {
+        vec3f_copy(&obj->oHomeVec, &obj->oPosVec);
+        obj->oBehParams2ndByte = POINT_BALLOON_25;
+        obj->oBehParams = POINT_BALLOON_25 << 16;
+        o->oRotatingBridgeBalloonChild = obj;
+    }
 }
 
 void bhv_rotating_bridge_loop(void) {
+    rotating_bridge_update_child_balloon();
     if (o->oAction == ROTATING_BRIDGE_IDLE || o->oTimer == 0) {
         return;
     }
 
-    o->oFaceAngleYaw += 0x10 * sqr(o->oTimer);
+    o->oRotatingBridgeYawRotated += (BRIDGE_MOVE_LENGTH / 0xC00) * sqr(o->oTimer);
 
-    if (o->oFaceAngleYaw > BRIDGE_MOVE_LENGTH) {
-        o->oFaceAngleYaw = BRIDGE_MOVE_LENGTH;
+    if (o->oRotatingBridgeYawRotated >= BRIDGE_MOVE_LENGTH) {
+        o->oRotatingBridgeYawRotated = BRIDGE_MOVE_LENGTH;
         o->oAction = ROTATING_BRIDGE_IDLE;
         cur_obj_play_sound_2(SOUND_GENERAL_ELEVATOR_LAND);
-        rotating_bridge_move_tmp_for_mist_particles();
     } else {
         cur_obj_play_sound_2(SOUND_ENV_METAL_BOX_PUSH);
     }
+
+    o->oFaceAngleYaw = o->oRotatingBridgeYawHome + o->oRotatingBridgeYawRotated;
 }
