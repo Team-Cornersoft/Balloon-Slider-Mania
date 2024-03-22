12093b1c12dff630e5609a10afd1d9742c1169eb
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-03-22 03:44:28 +0100

    C3 warps not working within areas. :c

--- levels/jrb/area_1/geo.inc.c
+++ levels/jrb/area_1/geo.inc.c
@@ -43,7 +43,6 @@ const GeoLayout jrb_dl_1_area1_geo[] = {
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 1300, -7750, -18500, jrb_dl_B_Mushrooms_c_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_ALPHA, -30735, 290, -1417, jrb_dl_B_Mushrooms_flat_a_mesh_layer_4),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_ALPHA, 30111, -1260, -3357, jrb_dl_B_Mushrooms_flat_b_mesh_layer_4),
-		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, -12300, -1300, 13000, jrb_dl_Bouncy_shroom_stem_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_ALPHA, -3476, -4212, 8278, jrb_dl_Bridge_a_mesh_layer_4),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_ALPHA, 9223, -7928, -10962, jrb_dl_Bridge_b_mesh_layer_4),
 		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, -1100, -9500, 8000, -15, 90, 0, jrb_dl_C_pipe_mesh_layer_1),
