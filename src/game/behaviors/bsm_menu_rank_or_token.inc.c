cdccab59f9ffa113f652552355a04d0cc5a96894
Gregory Heskett <gheskett@gmail.com>
2024-03-26 00:09:56 -0400

    Score balancing for first 4 courses

--- src/game/behaviors/bsm_menu_rank_or_token.inc.c
+++ src/game/behaviors/bsm_menu_rank_or_token.inc.c
@@ -2,16 +2,16 @@
 #include "game/save_file.h"
 
 static const u16 BSMRankMinimumScores[BSM_COURSE_COUNT][6] = {
-/*                                       D    C    B    A    S    G    */
-    [BSM_COURSE_1_SNOWY_PEAK]        = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_2_LAVA_ISLE]         = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_3_FUNGI_CANYON]      = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_4_STARLIGHT_FEST]    = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_5_HOLIDAY_PEAK]      = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_6_SCORCH_ISLE]       = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_7_SPORE_CANYON]      = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_8_CYBER_FEST]        = {100, 200, 300, 400, 500, 1000},
-    [BSM_COURSE_9_CORNERSOFT_PARADE] = {100, 200, 300, 400, 500, 1000},
+/*                                        D     C     B     A     S     G    */
+    [BSM_COURSE_1_SNOWY_PEAK]        = {1960, 2128, 2296, 2464, 2632, 2800}, // G * (1.0 - 0.06n)
+    [BSM_COURSE_2_LAVA_ISLE]         = {1340, 1480, 1600, 1700, 1780, 1840}, // G - (40 + 20n)
+    [BSM_COURSE_3_FUNGI_CANYON]      = {1344, 1505, 1667, 1828, 1989, 2150}, // G * (1.0 - 0.075n)
+    [BSM_COURSE_4_STARLIGHT_FEST]    = {1435, 1558, 1681, 1804, 1927, 2050}, // G * (1.0 - 0.06n)
+    [BSM_COURSE_5_HOLIDAY_PEAK]      = { 100,  200,  300,  400,  500, 1000},
+    [BSM_COURSE_6_SCORCH_ISLE]       = { 100,  200,  300,  400,  500, 1000},
+    [BSM_COURSE_7_SPORE_CANYON]      = { 100,  200,  300,  400,  500, 1000},
+    [BSM_COURSE_8_CYBER_FEST]        = { 100,  200,  300,  400,  500, 1000},
+    [BSM_COURSE_9_CORNERSOFT_PARADE] = { 100,  200,  300,  400,  500, 1000},
 };
 
 s32 calculate_bsm_rank(s32 courseNum, s32 score) {
