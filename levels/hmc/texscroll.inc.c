4f2ef50338966ddf7ca39a1cc2228567c8c75639
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-03-31 06:52:44 +0200

    C7 done

--- levels/hmc/texscroll.inc.c
+++ levels/hmc/texscroll.inc.c
@@ -82,14 +82,14 @@ void scroll_hmc_dl_C_sart_line_mesh_layer_5_vtx_2() {
 	currentY += deltaY;
 }
 
-void scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_0() {
+void scroll_hmc_dl_B_Dragon_exit_a2_mesh_layer_6_vtx_0() {
 	int i = 0;
-	int count = 96;
-	int height = 64 * 0x20;
+	int count = 24;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(hmc_dl_C_Finish_line_mesh_layer_5_vtx_0);
+	Vtx *vertices = segmented_to_virtual(hmc_dl_B_Dragon_exit_a2_mesh_layer_6_vtx_0);
 
 	deltaY = (int)(1.0 * 0x20) % height;
 
@@ -103,14 +103,14 @@ void scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_0() {
 	currentY += deltaY;
 }
 
-void scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_1() {
+void scroll_hmc_dl_B_Dragon_portal_a2_mesh_layer_6_vtx_0() {
 	int i = 0;
-	int count = 96;
-	int height = 64 * 0x20;
+	int count = 24;
+	int height = 32 * 0x20;
 
 	static int currentY = 0;
 	int deltaY;
-	Vtx *vertices = segmented_to_virtual(hmc_dl_C_Finish_line_mesh_layer_5_vtx_1);
+	Vtx *vertices = segmented_to_virtual(hmc_dl_B_Dragon_portal_a2_mesh_layer_6_vtx_0);
 
 	deltaY = (int)(1.0 * 0x20) % height;
 
@@ -124,25 +124,67 @@ void scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_1() {
 	currentY += deltaY;
 }
 
-void scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_2() {
+void scroll_hmc_dl_B_Dragon_portal_a2_001_mesh_layer_6_vtx_0() {
 	int i = 0;
-	int count = 36;
-	int width = 128 * 0x20;
+	int count = 24;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
-	Vtx *vertices = segmented_to_virtual(hmc_dl_C_Finish_line_mesh_layer_5_vtx_2);
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl_B_Dragon_portal_a2_001_mesh_layer_6_vtx_0);
 
-	deltaX = (int)(1.0 * 0x20) % width;
+	deltaY = (int)(1.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
+}
+
+void scroll_hmc_dl_B_Dragon_exit_a3_mesh_layer_6_vtx_0() {
+	int i = 0;
+	int count = 24;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl_B_Dragon_exit_a3_mesh_layer_6_vtx_0);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_hmc_dl_B_Dragon_exit_a3_001_mesh_layer_6_vtx_0() {
+	int i = 0;
+	int count = 24;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(hmc_dl_B_Dragon_exit_a3_001_mesh_layer_6_vtx_0);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
 }
 
 void scroll_gfx_mat_hmc_dl_Flow_a_layer5() {
@@ -192,9 +234,11 @@ void scroll_hmc() {
 	scroll_hmc_dl_C_sart_line_mesh_layer_5_vtx_0();
 	scroll_hmc_dl_C_sart_line_mesh_layer_5_vtx_1();
 	scroll_hmc_dl_C_sart_line_mesh_layer_5_vtx_2();
-	scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_0();
-	scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_1();
-	scroll_hmc_dl_C_Finish_line_mesh_layer_5_vtx_2();
+	scroll_hmc_dl_B_Dragon_exit_a2_mesh_layer_6_vtx_0();
+	scroll_hmc_dl_B_Dragon_portal_a2_mesh_layer_6_vtx_0();
+	scroll_hmc_dl_B_Dragon_portal_a2_001_mesh_layer_6_vtx_0();
+	scroll_hmc_dl_B_Dragon_exit_a3_mesh_layer_6_vtx_0();
+	scroll_hmc_dl_B_Dragon_exit_a3_001_mesh_layer_6_vtx_0();
 	scroll_gfx_mat_hmc_dl_Flow_a_layer5();
 	scroll_gfx_mat_hmc_dl_Flow_b_layer5();
 	scroll_gfx_mat_hmc_dl_Water_a_layer5();
