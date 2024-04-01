2d9994a650bd0ac63d9b2a451d3f9cef74aef7d5
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-04-01 08:49:42 +0200

    CI4

--- object_exports/balloon_pop/geo_header.h
+++ object_exports/balloon_pop/geo_header.h
@@ -1,5 +1,5 @@
 extern const GeoLayout balloon_pop_geo[];
-extern u8 balloon_pop_boom_ia8[];
+extern u8 balloon_pop_boom_ia4[];
 extern Vtx balloon_pop_model_mesh_layer_4_vtx_cull[8];
 extern Vtx balloon_pop_model_mesh_layer_4_vtx_0[4];
 extern Gfx balloon_pop_model_mesh_layer_4_tri_0[];
