05e061dceb345dc2a1c68741593a869f2be17454
MelroyOldenburger <15652953+MelroyOldenburger@users.noreply.github.com>
2024-04-01 08:51:56 +0200

    Ci4 fr

--- object_exports/balloon_pop/geo.inc.c
+++ object_exports/balloon_pop/geo.inc.c
@@ -3,7 +3,7 @@
 const GeoLayout balloon_pop_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_ALPHA, balloon_pop_model_mesh_layer_4),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, balloon_pop_Plane_mesh_layer_4),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
--- object_exports/balloon_pop/geo_header.h
+++ object_exports/balloon_pop/geo_header.h
@@ -1,8 +1,9 @@
 extern const GeoLayout balloon_pop_geo[];
-extern u8 balloon_pop_boom_ia4[];
-extern Vtx balloon_pop_model_mesh_layer_4_vtx_cull[8];
-extern Vtx balloon_pop_model_mesh_layer_4_vtx_0[4];
-extern Gfx balloon_pop_model_mesh_layer_4_tri_0[];
-extern Gfx mat_balloon_pop_Balloon_Pop[];
-extern Gfx mat_revert_balloon_pop_Balloon_Pop[];
-extern Gfx balloon_pop_model_mesh_layer_4[];
+extern u8 balloon_pop_boom_ci4[];
+extern u8 balloon_pop_boom_pal_rgba16[];
+extern Vtx balloon_pop_Plane_mesh_layer_4_vtx_cull[8];
+extern Vtx balloon_pop_Plane_mesh_layer_4_vtx_0[4];
+extern Gfx balloon_pop_Plane_mesh_layer_4_tri_0[];
+extern Gfx mat_balloon_pop_f3dlite_material[];
+extern Gfx mat_revert_balloon_pop_f3dlite_material[];
+extern Gfx balloon_pop_Plane_mesh_layer_4[];
