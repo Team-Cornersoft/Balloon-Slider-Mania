ba655826facd33f3c22373cf4ff0b7f89e53b509
Gregory Heskett <gheskett@gmail.com>
2024-05-28 19:32:54 -0400

    Time trials icon + animation

--- levels/castle_grounds/custom_menu_title/geo.inc.c
+++ levels/castle_grounds/custom_menu_title/geo.inc.c
@@ -1,9 +1,27 @@
 #include "src/game/envfx_snow.h"
 
+const GeoLayout custom_menu_title_01_title_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, custom_menu_title_01_title_mesh_layer_5),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
+const GeoLayout custom_menu_title_02time_trials_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, 0, -20, 0, custom_menu_title_02time_trials_mesh_layer_5),
+	GEO_CLOSE_NODE(),
+	GEO_RETURN(),
+};
 const GeoLayout custom_menu_title_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, custom_menu_title_model_mesh_layer_5),
+		GEO_SWITCH_CASE(2, geo_switch_anim_state),
+		GEO_OPEN_NODE(),
+			GEO_BRANCH(1, custom_menu_title_01_title_geo),
+			GEO_BRANCH(1, custom_menu_title_02time_trials_geo),
+		GEO_CLOSE_NODE(),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
--- src/game/behaviors/bsm_menu_title.inc.c
+++ src/game/behaviors/bsm_menu_title.inc.c
@@ -3,19 +3,42 @@
 #define MAX_SCALE_DIFF 0.02f
 #define BASE_SCALE 0.725f
 
+static void bhv_bsm_menu_title_update_scale(void) {
+    if (o->oAnimState == (u8) gBSMGameplayMode) {
+        o->oBSMMenuTitleGeneralScale = smoothstop(o->oBSMMenuTitleGeneralScale, 1.0f, 0.35f);
+    } else {
+        o->oBSMMenuTitleGeneralScale = smoothstop(o->oBSMMenuTitleGeneralScale, 0.0f, 0.35f);
+    }
+
+    if (o->oBSMMenuTitleGeneralScale <= 0.005f) {
+        o->oBSMMenuTitleGeneralScale = 0.005f;
+        cur_obj_hide();
+        o->oAnimState = gBSMGameplayMode;
+    } else {
+        cur_obj_unhide();
+    }
+}
+
 void bhv_bsm_menu_title_init(void) {
     cur_obj_scale(BASE_SCALE);
 
     o->oBSMMenuTitleRollHome = o->oFaceAngleRoll;
+    o->oAnimState = gBSMGameplayMode;
+    o->oBSMMenuTitleGeneralScale = 1.0f;
 }
 
 void bhv_bsm_menu_title_loop(void) {
     f32 sinCalc = sins(65536.0f * ((f32) o->oBSMMenuTitleSineFunc / (f32) SINE_FRAMES));
     f32 halfSinCalc = sins(65536.0f * ((f32) o->oBSMMenuTitleSineFunc / (f32) (SINE_FRAMES / 2)));
-    cur_obj_scale(BASE_SCALE + (MAX_SCALE_DIFF * halfSinCalc));
+    f32 scale = BASE_SCALE + (MAX_SCALE_DIFF * halfSinCalc);
     o->oFaceAngleRoll = o->oBSMMenuTitleRollHome + (MAX_ROTATION_OFFSET * sinCalc);
 
     o->oBSMMenuTitleSineFunc = (o->oBSMMenuTitleSineFunc + 1) % SINE_FRAMES;
 
     o->oPosX = o->oHomeX + BSM_MENU_CAMERA_LAYOUT_OFFSET;
+
+    bhv_bsm_menu_title_update_scale();
+
+    scale *= o->oBSMMenuTitleGeneralScale;
+    cur_obj_scale(scale);
 }
